84c19f6a6a8497f92048615bff46a4cdff4844d4
Dawid Janczak <dawid.janczak@gmail.com>
2012-09-09 17:05:54 +0200

    Initial formosa commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formosa::Application.config.secret_token = '7c73c7db9a87d120aeb5cbf112754ab6ce40b179234c6498f66d574f8c2619bdd896199eac896ea49c41d430b4378aecf97dbd090bab869682f04dea6a343e3d'
