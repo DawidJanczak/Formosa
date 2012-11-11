db19411735ce2eb9152ab8c27e582253a02f5044
DawidJanczak <dawid.janczak@gmail.com>
2012-11-11 20:44:33 +0000

    User can add posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,16 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(params[:post])
+    if @post.save
+      flash[:success] = "Post created."
+    end
+    redirect_to home_path
+  end
 end
