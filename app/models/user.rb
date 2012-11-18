504f13f35ab2fa7e4b992b34d771d3b6f3e2406d
DawidJanczak <dawid.janczak@gmail.com>
2012-11-18 19:03:16 +0000

    Sign up users with email and username, login with username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   validates :username, presence: true, uniqueness: true
 
@@ -15,12 +15,4 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase}]).first
-    else
-      where(conditions).first
-    end
-  end
 end
